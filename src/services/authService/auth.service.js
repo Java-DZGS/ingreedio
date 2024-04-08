266f944c09c41f18b8fd5a363164fdd9946560bf
Marcin Słupczyński <marcinxslup@gmail.com>
2024-04-08 23:02:54 +0200

    lil refactor

--- src/services/authService/auth.service.js
+++ src/services/authService/auth.service.js
@@ -1,8 +1,14 @@
 import axios from 'axios';
 import { apiUrl } from '../../config/config';
 
-export const signInApi = (userName, password) => axios.post(`${apiUrl}/auth/login`, { userName, password });
+const authApiUrl = `${apiUrl}/auth`;
 
-export const signUpApi = (userName, displayName, email, password) => axios.post(`${apiUrl}/auth/register`, {
-  userName, displayName, email, password,
-});
+export const signInApi = (userName, password) => axios.post(
+  `${authApiUrl}/login`, { userName, password },
+);
+
+export const signUpApi = (userName, displayName, email, password) => axios.post(
+  `${authApiUrl}/register`, {
+    userName, displayName, email, password,
+  },
+);
