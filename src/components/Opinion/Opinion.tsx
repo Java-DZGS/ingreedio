ae933995c80f217c585826f97ccc82480922f505
jdudz <jdudzinska625@gmail.com>
2024-05-18 17:46:25 +0200

    Opinion adding

--- src/components/Opinion/Opinion.tsx
+++ src/components/Opinion/Opinion.tsx
@@ -1,29 +1,44 @@
-// Opinion.js
+/* eslint-disable react/jsx-one-expression-per-line */
 import React from 'react';
 import './Opinion.scss';
 import { FaThumbsUp, FaThumbsDown, FaStar } from 'react-icons/fa';
 
-const Opinion = () => (
+interface OpinionProps {
+  username: string;
+  rating: number;
+  date: string;
+  content: string;
+  onLike: () => void;
+  onDislike: () => void;
+  onReport: () => void;
+}
+
+const Opinion = ({
+  username,
+  rating,
+  date,
+  content,
+  onLike,
+  onDislike,
+  onReport,
+}: OpinionProps): JSX.Element => (
   <div className="opinion">
     <div className="header">
       <div className="user-info">
-        <span className="username">JohnDoe</span>
-        <span className="rating">· 8/10 </span>
+        <span className="username">{username}</span>
+        <span className="rating">· {rating / 2}/5 </span>
         <FaStar className="star" />
       </div>
-      <span className="date">2024-05-17</span>
+      <span className="date">{date}</span>
     </div>
-    <div className="content">
-      This is an example opinion content. It is just a placeholder for the
-      actual opinion text.
-    </div>
+    <div className="content">{content}</div>
     <div className="footer">
-      <button className="report-button" type="button">
+      <button className="report-button" type="button" onClick={onReport}>
         Report
       </button>
       <div className="icons">
-        <FaThumbsUp className="icon" />
-        <FaThumbsDown className="icon" />
+        <FaThumbsUp className="icon" onClick={onLike} />
+        <FaThumbsDown className="icon" onClick={onDislike} />
       </div>
     </div>
   </div>
---
+++ src/components/StarRatingInput/StarRatingInput.tsx
@@ -0,0 +1,40 @@
+// StarRatingInput.tsx
+
+import React, { useState } from 'react';
+import { FaStar } from 'react-icons/fa';
+import './StarRatingInput.scss';
+
+interface StarRatingInputProps {
+  onChange: (rating: number) => void;
+}
+
+const StarRatingInput: React.FC<StarRatingInputProps> = ({ onChange }) => {
+  const [hover, setHover] = useState<number>(0);
+  const [selectedRating, setSelectedRating] = useState<number>(0);
+
+  const handleStarClick = (rating: number) => {
+    setSelectedRating(rating);
+    onChange(rating);
+  };
+
+  return (
+    <div className="star-container">
+      {[...Array(5)].map((_, index) => {
+        const rating = index + 1;
+        return (
+          <FaStar
+            key={index}
+            size={24}
+            color={rating <= (hover || selectedRating) ? '#ffc107' : '#e4e5e9'}
+            style={{ marginRight: 10, cursor: 'pointer' }}
+            onMouseEnter={() => setHover(rating)}
+            onMouseLeave={() => setHover(0)}
+            onClick={() => handleStarClick(rating)}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default StarRatingInput;
