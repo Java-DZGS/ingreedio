2f58a0bc689c79fe2a0d5c450c58bc6f0819d464
Marcin Słupczyński <marcinxslup@gmail.com>
2024-06-06 17:28:25 +0200

    Deleted todo

--- src/store/actions/auth.action.ts
+++ src/store/actions/auth.action.ts
@@ -24,7 +24,6 @@ const signInSuccess = (response: AuthResponse): AnyAction => ({
   payload: response,
 });
 
-// todo: error type // i think this is resolved
 const signInFailure = (error: AxiosError): AnyAction => ({
   type: types.SIGN_IN_FAILURE,
   payload: error,
